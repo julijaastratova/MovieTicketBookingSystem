f8fec1c0d3afa2608f7680e4195b93257b9ea09c
julijaastratova <j.sitkovska@gmail.com>
2021-12-08 20:16:20 +0200

    Add project files.

---
+++ MovieTicketBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieTicketBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
