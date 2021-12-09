544811a8e12dd3e6b2a60fceb24e05394b60e7a3
spikemarina152@gmail.com <spikemarina152@gmail.com>
2021-12-09 10:58:54 +0200

    Library

---
+++ MTBS.Library/ITicketSystemOp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTBS.Library
+{
+    interface ITicketSystemOp
+    {
+        public void ChooseFromCatalog();
+        public void ChooseDateTime();
+        public void ShooseSeat();
+        public void ChooseTicketType();
+        public void CheckAvailability();
+        public void BuyTicket();
+        public string PrintTicket();
+
+
+    }
+}
--- MovieTicketBookingSystem/Program.cs
+++ MovieTicketBookingSystem/Program.cs
@@ -22,6 +22,6 @@ namespace MovieTicketBookingSystem
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        //comment
+        
     }
 }
